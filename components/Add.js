925ccd06f0c6a5feb3689728969af225870c744b
Stanislav Popadynets <popadynetss@gmail.com>
2021-01-09 14:19:29 +0200

    Add ability to take pictures

--- components/Add.js
+++ components/Add.js
@@ -1,19 +1,85 @@
-import React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import React, {PureComponent, useState, useEffect, useRef} from 'react';
+import {Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {RNCamera} from 'react-native-camera';
+import Permissions from 'react-native-permissions';
 
 const Add = () => {
-  return (
+  const [flash, setFlash] = useState('off');
+  const [zoom, setZoom] = useState(0);
+  const [autoFocus, setAutoFocus] = useState('on');
+  const [depth, setDepth] = useState(0);
+  const [type, setType] = useState('back');
+  const [permission, setPermission] = useState('undetermined');
+  const [imageURI, setImageURI] = useState('');
+
+  const cameraRef = useRef(null);
+  useEffect(() => {
+    Permissions.check('photo').then((response) => {
+    // Response is one of: 'authorized', 'denied', 'restricted', or 'undetermined'
+    setPermission(response);
+    });
+  }, []);
+
+  const toggleFlash = () => {
+    setFlash(flashModeOrder[flash]);
+  }
+  const zoomOut = () => {
+    setZoom(zoom - 0.1 < 0 ? 0 : zoom - 0.1);
+  }
+  const zoomIn = () => {
+    setZoom(zoom + 0.1 > 1 ? 1 : zoom + 0.1);
+  }
+  const takePicture = async () => {
+    if (cameraRef) {
+      const options = {quality: 0.5, base64: true};
+      const data = await cameraRef.current.takePictureAsync(options);
+      console.log(data.uri);
+      setImageURI(data.uri);
+    }
+  };
+  if(!imageURI) {
+    return (
     <View style={styles.container}>
-      <Text>Add</Text>
+      <RNCamera
+        ref={cameraRef}
+        style={styles.preview}
+        type={type}
+        flashMode={flash}
+      />
+      <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
+        <TouchableOpacity onPress={takePicture} style={styles.capture}>
+          <Text style={{ fontSize: 14 }}> SNAP </Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
+}
+if(imageURI) {
+  return (
+    <Image source={{ uri: imageURI }} style={{flex: 1}} />
+  )
+}
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    flexDirection: 'column',
+    backgroundColor: 'black',
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
     alignItems: 'center',
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    padding: 15,
+    paddingHorizontal: 20,
+    alignSelf: 'center',
+    margin: 20,
   },
 });
 
